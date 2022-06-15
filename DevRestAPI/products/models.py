b0ba01a35a5e76703e2e4a536201f36ef939b109
Manohar Upputuri <saimanohar.upputuri@gmail.com>
2022-06-15 10:11:52 +0530

    Added Chapter2

--- DevRestAPI/products/models.py
+++ DevRestAPI/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductModel(models.Model):
+	id = models.IntegerField(auto_created=True, primary_key=True)
+	title = models.CharField(max_length=20)
+	category = models.TextField(blank=True, null=True)
+	price = models.DecimalField(max_digits=15, decimal_places=10, default=99.99)
+
+
--- DevRestAPI/products/views.py
+++ DevRestAPI/products/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
+from . models import ProductModel
+from django.forms.models import model_to_dict
 # Create your views here.
 
 
 def product(request, *args, **kwargs):
-	# Receiving the data from the client - body , headers , params 
-	body = request.body
+	# Receiving the data from model
+	product_model_data = ProductModel.objects.all().order_by("?").first() # random query set
+	"""
 	data = {}
-	print(body)
-	try:
-		data = json.loads(body)
-	except:
-		pass
-	#return JsonResponse({"message": "Success"}
+	if product_model_data:
+		data['id'] = product_model_data.id
+		data['title'] = product_model_data.title
+		data['category'] = product_model_data.category
+		data['price'] = product_model_data.price
+	"""
+	# serilization model instance to python dict 
+	data = {}
+	if product_model_data:
+		data = model_to_dict(product_model_data, fields=['id', 'title'])
 	#return JsonResponse(data)
-	data['headers'] = dict(request.headers)
-	data['content_type'] = request.content_type
-	data['params'] = dict(request.GET)
-	return JsonResponse(data)
-
-
+	return HttpResponse(json.dumps(data), headers={"content-type": "application/json"})
+	
